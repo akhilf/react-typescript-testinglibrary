521b4d5900b21cba94d2af5504bd4fa5c16c264d
Akhil Francis <akfranci@cisco.com>
2021-07-04 10:05:27 +0530

    Test library first commit

--- src/pages/About.test.tsx
+++ src/pages/About.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import userEvent from '@testing-library/user-event';
 import { render } from '../../test/test-utils';
-
 import About from './About';
 
 it('renders <About /> page', () => {
   // You should be able to verify the About page rendered correctly.
+  // const div = document.createElement("div");
+  // ReactDOM.render(<About></About>, div);
 });
 
 it('clicks button and fires increment counter', () => {
--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import userEvent from '@testing-library/user-event';
 import { render } from '../../test/test-utils';
-
 import Home from './Home';
 
 it('renders <Home /> page', () => {
   // You should be able to show that you can verify Home rendered correctly
+    const div = document.createElement("div");
+    ReactDOM.render(<Home></Home>, div);
 });
